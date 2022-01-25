1c3cb9f95fc897493e6bdaee1a7cde48dfa48fa5
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2022-01-25 22:34:49 +0100

    börja lägga till själva naven och register

--- app.rb
+++ app.rb
@@ -5,6 +5,40 @@ require 'bcrypt'
 
 enable :sessions
 
+
+# GET called
+
 get('/') do
     slim(:start)
 end
+
+get('/showregister') do
+    slim(:register)
+end
+
+get('/showlogin') do
+    slim(:login)
+end
+
+get('/posts') do
+    slim(:"posts/index")
+end
+
+# POST called
+
+post('/register') do
+    username = params[:username]
+    password = params[:password]
+    passwordConfirm = params[:passwordConfirm]
+  
+    if password == passwordConfirm
+        passwordDigest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new("db/database.db")
+        db.execute("INSERT INTO users (username, pwdigest) VALUES (?,?)", username, passwordDigest).first
+        session[:auth] = true
+        redirect('/posts')
+    else
+        session[:registerError] = true
+        redirect('/showregister')
+    end
+  end
