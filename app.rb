2ee2297f24213a3c40310363d263a5425c9b8b56
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2022-01-27 21:31:49 +0100

    La till login men funkar ej

--- app.rb
+++ app.rb
@@ -39,9 +39,32 @@ post('/register') do
         db = SQLite3::Database.new("db/database.db")
         db.execute("INSERT INTO users (username, pwdigest, email, phonenumber, birthday) VALUES (?,?,?,?,?)", username, passwordDigest, email, phonenumber, birthday).first
         session[:auth] = true
+        session[:user] = username
         redirect('/posts')
     else
         session[:registerError] = true
         redirect('/showregister')
     end
-  end
+end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+  
+    db = SQLite3::Database.new("db/datebase.db")
+    db.results_as_hash = true
+  
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(pwdigest) == password
+        # session[:id] = id
+        session[:auth] = true
+        session[:user] = username
+        redirect('/posts')
+      else
+        session[:loginError] = true
+        redirect('/showlogin')
+      end
+end
