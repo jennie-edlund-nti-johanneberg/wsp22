9c49cdd9eb01c9fd9e3c17db636f7271ba934f6a
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2022-02-22 15:23:54 +0100

    påbörjat en like knapp

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,15 +1,21 @@
-// window.onscroll = function() {scrollFunction()};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//       console.log('lol')
-//     // document.getElementById("nav").style.height = "20vh";
-//     document.getElementById("nav").classList.add("shrink")
-// } else {
-//     console.log('lol2')
-//     document.getElementById("nav").classList.remove("shrink")
-//     // document.getElementById("nav").style.height = "auto";
-//   }
-// }
 
-// console.log("hello")
+let hearts = document.querySelectorAll('.likeHeart')
+let forms = document.querySelectorAll('.likeForm')
+
+hearts.forEach((heart, index) => {
+    heart.addEventListener('click', () => {
+        forms[index].submit()
+
+        if(heart.src === "http://localhost:4567/img/heart.png"){
+            // Like
+            heart.src = "./img/heart_red.png"
+        }else{
+            // Unlike
+            heart.src = "./img/heart.png"
+
+        }
+    })
+
+})
+
