bb36bc722c935c4b8ce200f4cc6bbc4750adba00
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2022-02-22 15:23:07 +0100

    fixat css till scss och lärt mig lite javascript

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,1 +1,15 @@
-document.getElementById("demo").innerHTML = "Hello JavaScript!";
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//       console.log('lol')
+//     // document.getElementById("nav").style.height = "20vh";
+//     document.getElementById("nav").classList.add("shrink")
+// } else {
+//     console.log('lol2')
+//     document.getElementById("nav").classList.remove("shrink")
+//     // document.getElementById("nav").style.height = "auto";
+//   }
+// }
+
+// console.log("hello")
