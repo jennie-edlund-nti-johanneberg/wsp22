eed1783a91e92b1b5beaa16a55b549ce4edd1c64
jennie-edlund-nti-johanneberg <74712753+jennie-edlund-nti-johanneberg@users.noreply.github.com>
2022-03-21 15:56:45 +0100

    deletepopup klar

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,12 +1,36 @@
 let hearts = document.querySelectorAll('.likeHeart')
-let forms = document.querySelectorAll('.likeForm')
+let formsHearts = document.querySelectorAll('.likeForm')
 
 hearts.forEach((heart, index) => {
     
     heart.addEventListener('click', () => {
         // console.log('click')
-        forms[index].submit()
+        formsHearts[index].submit()
     })
 })
 
+let deleteBtn = document.querySelectorAll('.deleteButton')
+let formsDelete = document.querySelectorAll('.modalContent')
+
+
+deleteBtn.forEach((eachdelete, index) => {
+    const modalDiv = document.querySelectorAll('#modalDiv')[index]
+    
+    eachdelete.addEventListener('click', () => {
+        deleteBtn[index].style.display = 'none'
+        modalDiv.style.display = 'block'
+
+        let cancelBtn = document.getElementsByClassName('cancelBtn')[index]
+        let confirmDelete = document.getElementsByClassName('confirmDelete')[index]
+
+        cancelBtn.addEventListener('click', () => {
+            modalDiv.style.display = 'none'
+            deleteBtn[index].style.display = 'block'
+        })
+
+        confirmDelete.addEventListener('click', () => {
+            formsDelete[index].submit()
+        })
+    })
+})
 
